6c08a250c7612db64d3b1ad2e0ab2c8ea29d9bed
Jessica Benito Madrigal <jessbnmd@gmail.com>
2022-04-29 19:38:58 -0230

    First version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, Input, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'portfolio';
+  currentSelection = 'home';
+  constructor(
+  ) { }
+  ngOnInit():void { }
+
+  @HostListener('scroll') onContainerScroll(e: Event): void {
+    let sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+      let sectionTop = section.offsetTop;
+      if (this.getYPosition(e) >= sectionTop) {
+        this.currentSelection = section.id;
+      }
+
+    })
+  }
+
+  getYPosition(e: Event): number {
+    return (e.target as Element).scrollTop + 8;
+  }
 }
